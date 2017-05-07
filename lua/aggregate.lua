b0bbbc690a73f019e059aaa0d8bb3094f47a7e15
Smita <smita@Smitas-MacBook-Pro.local>
2017-05-07 15:33:10 +0530

    createClaimData.go

--- lua/aggregate.lua
+++
@@ -1,9 +0,0 @@
-function count(s)
-    function mapper(rec)
-            return 1
-    end
-    local function reducer(v1, v2)
-        return v1 + v2
-    end
-    return s : map(mapper) : reduce(reducer)
-end
